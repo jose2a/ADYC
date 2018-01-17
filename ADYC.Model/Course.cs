b540f57855f24260e31321dcfd2c45f33e542ca6
j2arriaga <josearriag@hotmail.com>
2018-01-16 19:03:12 -0600

    Added Course and CourseType classes.

---
+++ ADYC.Model/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public int CourseTypeId { get; set; }
+        public CourseType CourseType { get; set; }
+
+        public ICollection<Offering> Offerings { get; set; }
+
+        public Course()
+        {
+            Offerings = new List<Offering>();
+        }
+    }
+}
