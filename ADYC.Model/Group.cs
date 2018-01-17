94384ad13328e279f3122c15678059e87bbe37a4
j2arriaga <josearriag@hotmail.com>
2018-01-16 19:17:00 -0600

    Added GradeLetter enum and Group class.

---
+++ ADYC.Model/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.Model
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+        public Group()
+        {
+            Students = new List<Student>();
+        }
+    }
+}
