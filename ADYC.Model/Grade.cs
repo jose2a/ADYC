3b9c255802eb0164572702de556a9040b533ee69
j2arriaga <josearriag@hotmail.com>
2018-01-16 19:13:45 -0600

    Added Grade class.

---
+++ ADYC.Model/Grade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.Model
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+        public Grade()
+        {
+            Students = new List<Student>();
+        }
+    }
+}
