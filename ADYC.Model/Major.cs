78e9a5b6e85a57a2bc98477000b07f03d0f0495b
j2arriaga <josearriag@hotmail.com>
2018-01-16 19:18:23 -0600

    Added Major class.

---
+++ ADYC.Model/Major.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.Model
+{
+    public class Major
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+        public Major()
+        {
+            Students = new List<Student>();
+        }
+    }
+}
