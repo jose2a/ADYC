d76cdb8f5061b519aa807a12b52be9bc8a335827
j2arriaga <josearriag@hotmail.com>
2018-01-16 19:10:20 -0600

    Added Evaluation class.

---
+++ ADYC.Model/Evaluation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.Model
+{
+    public class Evaluation
+    {
+        public int PeriodId { get; set; }
+        public int EnrollmentId { get; set; }
+
+        public decimal? PeriodGrade { get; set; }
+        public GradeLetter? PeriodGradeLetter { get; set; }
+        public string Notes { get; set; }
+
+        public Period Period { get; set; }
+        public Enrollment Enrollment { get; set; }
+    }
+}
