e853743ea4585b5c6de9bdf66484fd416db10644
j2arriaga <josearriag@hotmail.com>
2018-01-25 20:01:25 -0600

    Added repository contracts for the project's model.

---
+++ ADYC.IRepository/IGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using ADYC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.IRepository
+{
+    public interface IGroupRepository : IRepository<Group>
+    {
+    }
+}
