2d1a0ab7434651ed45ab061bf6414f7d1475d0f9
j2arriaga <josearriag@hotmail.com>
2018-02-03 14:14:33 -0600

    Added CourseTypeRepositoryand ICourseTypeService.

---
+++ ADYC.IService/ICourseTypeService.cs
@@ -0,0 +1,23 @@
+﻿using ADYC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.IService
+{
+    public interface ICourseTypeService
+    {
+        CourseType Get(int id);
+        IEnumerable<CourseType> GetAll();
+        IEnumerable<CourseType> FindByName(string name);
+
+        void Add(CourseType courseType);
+
+        void Update(CourseType courseType);
+
+        void Remove(CourseType courseType);
+        void RemoveRange(IEnumerable<CourseType> courseTypes);
+    }
+}
---
+++ ADYC.Repository/CourseTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using ADYC.IRepository;
+using ADYC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ADYC.Repository
+{
+    public class CourseTypeRepository : Repository<CourseType>, ICourseTypeRepository
+    {
+        public CourseTypeRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
