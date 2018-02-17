df8bd33481212cc7634b7305c6ce76dac3de06ae
j2arriaga <josearriag@hotmail.com>
2018-02-17 10:07:32 -0600

    Added IOfferingService interface.

---
+++ ADYC.IService/IOfferingService.cs
@@ -0,0 +1,34 @@
+﻿using ADYC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADYC.IService
+{
+    public interface IOfferingService
+    {
+        Offering Get(int id);
+                
+        IEnumerable<Offering> FindByTermName(string termName);
+        IEnumerable<Offering> FindByTermId(int termId);
+        IEnumerable<Offering> FindByCurrentTerm();
+        IEnumerable<Offering> FindByCourseName(string courseName);
+        IEnumerable<Offering> FindByCourseId(int courseId);
+        IEnumerable<Offering> FindByProfessorName(string professorName);
+        IEnumerable<Offering> FindByProfessorLastName(string professorLastName);
+        IEnumerable<Offering> FindByProfessorId(Guid professorId);
+        IEnumerable<Offering> FindByProfessorNameAndTermName(string professorName, string termName);
+        IEnumerable<Offering> FindByProfessorIdCourseIdAndTermId(Guid professorId, int courseId, int termId);
+        IEnumerable<Offering> FindByProfessorIdAndTermId(Guid professorId, int termId);
+        IEnumerable<Offering> FindByLocation(string location);
+
+        IEnumerable<Enrollment> FindEnrollmentsByOfferingId(int offeringId);
+
+        void Add(Offering offering);
+        void Update(Offering offering);
+
+        void Remove(Offering offering);
+    }
+}
