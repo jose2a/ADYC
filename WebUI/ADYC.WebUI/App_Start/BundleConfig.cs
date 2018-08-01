b78fa354ca7d0672db7d7b50f0da8d53c636ddac
j2arriaga <josearriag@hotmail.com>
2018-07-31 21:09:39 -0400

    Added bootbox for confirmation messages when calling delete and remove methods.

--- WebUI/ADYC.WebUI/App_Start/BundleConfig.cs
+++ WebUI/ADYC.WebUI/App_Start/BundleConfig.cs
@@ -21,7 +21,10 @@ namespace ADYC.WebUI
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootbox.js"));
+
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
